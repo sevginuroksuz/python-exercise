636dd0e1f8d54d3e6c3aecfdab68ac0526f18490
Sevgi Nur Öksüz <90787721+sevginuroksuz@users.noreply.github.com>
2023-03-21 23:12:13 +0300

    Delete Python-Exercises directory

--- Python-Exercises/Soru5.py
+++
@@ -1,9 +0,0 @@
-#Girilen 5 sayıdan en büyüğünü gösteren programın python kodlarını yazınız.
-sayilar=[]
-n=int(input("Kac adet sayi girilecek?:"))
-for i in range(n):
-    sayi=int(input("Sayi girin:"))
-    sayilar.append(sayi)
-en_kucuk=min(sayilar)
-en_buyuk=max(sayilar)
-print(f"Listedeki en buyuk sayi:{en_buyuk}\nListedeki en kucuk sayi:{en_kucuk}")
